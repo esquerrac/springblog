90445efa6b5d5e2ec78b8570ea2ff9e73f6eb2f2
chrisesquerra <esquerra>
2018-11-05 11:24:54 -0600

    authentication/security

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -20,17 +20,27 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/sign-up")
+    @GetMapping("/register")
     public String showSignupForm(Model model){
         model.addAttribute("user", new User());
-        return "users/sign-up";
+        return "users/register";
     }
 
-    @PostMapping("/sign-up")
+    @PostMapping("/register")
     public String saveUser(@ModelAttribute User user){
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         userSvc.save(user);
-        return "redirect:/login";
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(){
+        return "/posts";
     }
 }
