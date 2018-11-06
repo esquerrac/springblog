293cf099dd80daf51150cc1de4be6e905f86167d
chrisesquerra <esquerra>
2018-11-06 12:02:12 -0600

    deployment debugging

--- seeder.sql
+++ seeder.sql
@@ -1,135 +1,114 @@
 use blog_db;
 insert into users (username, email, password)
 values(
-'BigParrot',
-'BirdGuy@email.com',
-'birds'
-),(
-'DuckRainbow',
-'colors@email.com',
-'rainbow'
-),(
-'NobleFlea',
-'bug@email.com',
-'password'
-),(
-'TacoCat',
-'palindrome@email.com',
-'backwards'
-);
+          'BigParrot',
+          'BirdGuy@email.com',
+          'birds'
+          ),(
+          'DuckRainbow',
+          'colors@email.com',
+          'rainbow'
+          ),(
+          'NobleFlea',
+          'bug@email.com',
+          'password'
+          ),(
+          'TacoCat',
+          'palindrome@email.com',
+          'backwards'
+          );
 
 insert into posts(title, body, user_id)
 values(
-'Hook me up with that al pastor',
-'Every day is taco ipsum tuesday. It’s taco time all the time. Black or pinto beans? It’s raining tacos, from out of the sky, tacos, don’t even ask why. BARBACOA!! Yeah, apparently the taco shack was robbed. They left the money but took the tacos. Carne asada on corn tortillas. Flour or corn tortillas? BARBACOA!! Tacos al pastor made with adobada meat. Give me tacos, or give me death. I’ve been following that taco truck around all day. Give me all the tacos, immediately. Give me all your tacos.
-Give me tacos, or give me death. Pico de gallo, on the side please. Fish tacos with cabbage slaw and a side of chips and guac. Black or pinto beans? Give me tacos, or give me death. It’s taco Tuesday Monday. It’s taco time all the time. BARBACOA!! Make it a double there pal. 50 cent tacos! I’ll take 30. CARNE ASADA!! Say taco one more time. These tacos are lit 🔥. Can you put some peppers and onions on that? Pico de gallo, on the side please. It’s a wonderful morning for breakfast tacos.',
-1
-),(
-'Yum yum yum, delicioso!',
-'Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp. How bout a gosh darn quesadilla? Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp.
-
-CARNE ASADA!! Say taco one more time. Shrimp tacos are tasty tacos! It’s taco Tuesday Monday. Tacos al pastor made with adobada meat. How do you feel about hard shelled tacos? Flour or corn tortillas? It’s raining tacos, from out of the sky, tacos, don’t even ask why. You see, the refried beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell. Pico de gallo, on the side please. Ooh, with diced onions and a pinch of cilantro.',
-1
-),(
-'Tacos are delicious',
-'I think my favorite food is tacos.  Like, I am SUPER into tacos.  I may enjoy them more than any other human who has ever lived.  Every day is taco ipsum tuesday. It’s raining tacos, from out of the sky, tacos, don’t even ask why. Side of rice and beans, please. Pico de gallo, on the side please. Ooh, with diced onions and a pinch of cilantro. Tacos al pastor made with adobada meat. Make it a double there pal. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. BARBACOA!! 50 cent tacos! I’ll take 30. It’s raining tacos, from out of the sky, tacos, don’t even ask why. Yeah, apparently the taco shack was robbed. They left the money but took the tacos.',
-4
-),(
-'More tacos more problems',
-'Every day is taco ipsum tuesday. It’s a wonderful morning for breakfast tacos. Can you put some peppers and onions on that? Side of rice and beans, please. Tacos for breakfast, lunch and dinner. Flour or corn tortillas? CARNITAS!! 50 cent tacos! I’ll take 30. TACOS!! CARNE ASADA!! Yeah, apparently the taco shack was robbed. They left the money but took the tacos. CARNITAS!! Give me all your tacos. TACOS!! Flour or corn tortillas? I’ve been following that taco truck around all day. Add in a few el Pastor with guac and diced onions.
-
-It’s taco Tuesday Monday. Give me tacos, or give me death. You see, the refried beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell. Tacos, again? This will be 5 times this week and it’s only Tuesday. Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp. Make it a double there pal.
-
-Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Give me all the tacos, immediately. Does guac cost extra? It’s taco time all the time. Say taco one more time. Add in a few el Pastor with guac and diced onions. Tacos for breakfast, lunch and dinner. CARNITAS!! It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. Fish tacos with cabbage slaw and a side of chips and guac.
-
-Make it a double there pal. If you were a taco, would you eat yourself? It’s taco Tuesday Monday. These tacos are lit 🔥. It’s raining tacos, from out of the sky, tacos, don’t even ask why. I think I’ve overdosed on tacos. Josh’s taco shack is the best taco shack. Burritos are very tasty. CARNE ASADA!! Burritos are very tasty. Make it a double there pal. Does guac cost extra? CARNE ASADA!! Carne asada on corn tortillas. CARNITAS!! Can you put some peppers and onions on that? I’ve been following that taco truck around all day.',
-3
-),(
-'I''ve been chasing this taco truck all day.',
-'Every day is taco ipsum tuesday. How do you feel about hard shelled tacos? Can you put some peppers and onions on that? It’s a wonderful morning for breakfast tacos. These tacos are lit 🔥. Give me tacos, or give me death. CARNE ASADA!! Add in a few el Pastor with guac and diced onions. Add in a few el Pastor with guac and diced onions. 50 cent tacos! I’ll take 30. Give me all the tacos, immediately. I’d have to say, those tacos are on fleek. BARBACOA!! It’s taco time all the time. I’ve been following that taco truck around all day.',
-2
-),(
-'I found a new restaurant',
-'Every day is taco ipsum tuesday. Tacos, again? This will be 5 times this week and it’s only Tuesday. Pico de gallo, on the side please. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. If you were a taco, would you eat yourself? Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Say taco one more time. 50 cent tacos! I’ll take 30.
-
-Yeah, apparently the taco shack was robbed. They left the money but took the tacos. Let’s do a beef and a chicken, and one with both. Black or pinto beans? Black or pinto beans? Tacos al pastor made with adobada meat. How do you feel about hard shelled tacos? It’s taco time all the time. Give me tacos, or give me death. Give me all your tacos. Flour or corn tortillas? Tacos, again? This will be 5 times this week and it’s only Tuesday. Side of rice and beans, please.',
-4
-),(
-'The tacOdyssey',
-'Every day is taco ipsum tuesday. CARNE ASADA!! Add in a few el Pastor with guac and diced onions. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. Carne asada on corn tortillas. Give me tacos, or give me death. Give me all the tacos, immediately. CARNITAS!! Tacos for breakfast, lunch and dinner. Say taco one more time. Give me tacos, or give me death.
-
-BARBACOA!! Shrimp tacos are tasty tacos! Shrimp tacos are tasty tacos! Side of rice and beans, please. It’s taco time all the time. It’s taco Tuesday Monday. Let’s do a beef and a chicken, and one with both. Tacos, again? This will be 5 times this week and it’s only Tuesday. Burritos are very tasty. I’d have to say, those tacos are on fleek. These tacos are lit 🔥. You see, the refried beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell.
-
-Make it a double there pal. Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins. If you were a taco, would you eat yourself? Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins. How bout a gosh darn quesadilla?
-
-I’ve been following that taco truck around all day. Burritos are very tasty. BARBACOA!! It’s raining tacos, from out of the sky, tacos, don’t even ask why. I’d have to say, those tacos are on fleek. Give me all the tacos, immediately. Say taco one more time. Add in a few el Pastor with guac and diced onions. CARNE ASADA!! You see, the refried beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell. How do you feel about hard shelled tacos? Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins.',
-1
-),(
-'I''ve discovered a new recipe!',
-'Every day is taco ipsum tuesday. Tacos for breakfast, lunch and dinner. Say taco one more time. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Tacos, tacos, tacos. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Let’s do a beef and a chicken, and one with both. Can you put some peppers and onions on that? Ooh, with diced onions and a pinch of cilantro.',
-2
-),(
-'Taco Tuesday is a National holiday',
-'Every day is taco ipsum tuesday. Tacos, again? This will be 5 times this week and it’s only Tuesday. BARBACOA!! CARNE ASADA!! Burritos are very tasty. It’s taco time all the time. Give me all your tacos. Make it a double there pal. Carne asada on corn tortillas. It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. Give me all your tacos. If you were a taco, would you eat yourself? I think I’ve overdosed on tacos. It’s taco Tuesday Monday.
-
-I’d have to say, those tacos are on fleek. Add in a few el Pastor with guac and diced onions. Give me tacos, or give me death. Carne asada on corn tortillas. It’s taco Tuesday Monday. Does guac cost extra? Flour or corn tortillas? Give me all the tacos, immediately. Can you put some peppers and onions on that? You see, the refried beans are really just the adhesive necessary to apply the soft tortilla to the hard taco shell. Say taco one more time. I’ve been following that taco truck around all day. Josh’s taco shack is the best taco shack.
-
-50 cent tacos! I’ll take 30. It’s a wonderful morning for breakfast tacos. Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins. Black or pinto beans? I’d have to say, those tacos are on fleek. Give me tacos, or give me death. I’d have to say, those tacos are on fleek. Does guac cost extra? Tacos al pastor made with adobada meat. Give me all your tacos.',
-4
-);
+          'Hook me up with that al pastor',
+          'Every day is taco ipsum tuesday. It’s raining tacos, from out of the sky, tacos, don’t even ask why. Tacos al pastor made with adobada meat. Give me tacos, or give me death.',
+          1
+          ),(
+          'Yum yum yum, delicioso!',
+          'Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp.',
+          1
+          ),(
+          'Tacos are delicious',
+          'I think my favorite food is tacos.  Like, I am SUPER into tacos.  I may enjoy them more than any other human who has ever lived.  Every day is taco ipsum tuesday.',
+          4
+          ),(
+          'More tacos more problems',
+          'Every day is taco ipsum tuesday. It’s a wonderful morning for breakfast tacos. Can you put some peppers and onions on that? Side of rice and beans, please. Tacos for breakfast, lunch and dinner.',
+          3
+          ),(
+          'I''ve been chasing this taco truck all day.',
+          'Every day is taco ipsum tuesday. How do you feel about hard shelled tacos? Can you put some peppers and onions on that? It’s a wonderful morning for breakfast tacos. Give me tacos, or give me death.',
+          2
+          ),(
+          'I found a new restaurant',
+          'Every day is taco ipsum tuesday. Tacos, again? This will be 5 times this week and it’s only Tuesday. Pico de gallo, on the side please. Yeah, apparently the taco shack was robbed.',
+          4
+          ),(
+          'The tacOdyssey',
+          'Every day is taco ipsum tuesday. Add in a few el Pastor with guac and diced onions. It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business.',
+          1
+          ),(
+          'I''ve discovered a new recipe!',
+          'Every day is taco ipsum tuesday. Tacos for breakfast, lunch and dinner. Say taco one more time. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce.',
+          2
+          ),(
+          'Taco Tuesday is a National holiday',
+          'Tacos, again? This will be 5 times this week and it’s only Tuesday. BARBACOA!! CARNE ASADA!! Burritos are very tasty. It’s taco time all the time. Give me all your tacos. Make it a double there pal.',
+          4
+          );
 
 insert into categories(name)
 values(
-'Flour'),
-('Corn'),
-('Chicken'),
-('Beef'),
-('Pork'),
-('Vegetarian'),
-('Gluten Free'),
-('Spicy'
-);
+          'Flour'),
+      ('Corn'),
+      ('Chicken'),
+      ('Beef'),
+      ('Pork'),
+      ('Vegetarian'),
+      ('Gluten Free'),
+      ('Spicy'
+          );
 
 insert into posts_categories(post_id, category_id)
 values(
-1,2
-),(
-1,4
-),(
-2,2
-),(
-3,1
-),(
-3,5
-),(
-3,8
-),(
-4,8
-),(
-4,1
-),(
-4,3
-),(
-5,2
-),(
-5,7
-),(
-6,1
-),(
-7,2
-),(
-7,8
-),(
-8,1
-),(
-8,4
-),(
-8,7
-),(
-8,8
-),(
-9,1
-),(
-9,8
-);
+          1,2
+          ),(
+          1,4
+          ),(
+          2,2
+          ),(
+          3,1
+          ),(
+          3,5
+          ),(
+          3,8
+          ),(
+          4,8
+          ),(
+          4,1
+          ),(
+          4,3
+          ),(
+          5,2
+          ),(
+          5,7
+          ),(
+          6,1
+          ),(
+          7,2
+          ),(
+          7,8
+          ),(
+          8,1
+          ),(
+          8,4
+          ),(
+          8,7
+          ),(
+          8,8
+          ),(
+          9,1
+          ),(
+          9,8
+          );
 
